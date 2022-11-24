c3630a58f6353fd34771a05c951c620b46109bf9
Jamie Omondi <cruiseomondi90@gmail.com>
2022-11-24 13:02:01 +0300

    Switching to firebase kill me if you want

--- app/src/main/java/com/example/mytunes/MainActivity.kt
+++ app/src/main/java/com/example/mytunes/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.mytunes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import androidx.navigation.fragment.NavHostFragment
 import com.example.mytunes.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +12,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+
+//        Handler().postDelayed({
+//            navController.navigate(R.id.action_splashFragment_to_authFragment)
+//        }, 3000)
+        Handler().postDelayed({
+            navController.navigate(R.id.action_splashFragment_to_homeFragment)
+        }, 3000)
     }
 }
