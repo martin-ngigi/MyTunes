28c23af0d941f755b7bdaecbe39384d550b89e70
martin-ngigi <martinwainaina001@gmail.com>
2022-11-24 11:40:19 +0300

    Get data from UI

--- app/src/main/java/com/example/mytunes/fragments/RegisterFragment.kt
+++ app/src/main/java/com/example/mytunes/fragments/RegisterFragment.kt
@@ -1,4 +1,38 @@
 package com.example.mytunes.fragments
 
-class RegisterFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.mytunes.R
+import com.example.mytunes.databinding.FragmentSignUpBinding
+
+class RegisterFragment: Fragment() {
+
+    lateinit var binding: FragmentSignUpBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return layoutInflater.inflate(R.layout.fragment_sign_up, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding = FragmentSignUpBinding.bind(view)
+
+        //get data from UI
+        var first_name = binding.fName.editableText.toString()
+        var last_name = binding.lName.editableText.toString()
+        var email = binding.email.editableText.toString()
+        var phone = binding.phone.editableText.toString()
+        var password = binding.registerPassword.editableText.toString()
+
+    }
+
+
 }
